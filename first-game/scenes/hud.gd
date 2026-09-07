74fa0bf419e3607d57ba5a9b2435243d1a32df16
gangodawilae <gangodawilae@wsc.school.nz>
2026-09-07 13:53:43 +1200

    Score appear in the game when play

---
+++ first-game/scenes/hud.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@onready var score_label: Label = $CoinTexture/ScoreLabel
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func update_score(score):
+	score_label.text = str(score)
