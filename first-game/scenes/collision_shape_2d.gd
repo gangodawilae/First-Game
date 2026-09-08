f2f3a9c61e0e7ea4dfbed219a747d4c2decd8fdb
gangodawilae <gangodawilae@wsc.school.nz>
2026-09-08 14:13:54 +1200

    Changed the colors of the texts

--- first-game/scenes/collision_shape_2d.gd
+++ first-game/scenes/collision_shape_2d.gd
@@ -1,17 +1,20 @@
-extends CollisionShape2D
+extends Node2D
 
 const SPEED = 60
-
 var direction = 1
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta: float)-> void:
 	if ray_cast_right.is_colliding():
 		direction = -1
+		animated_sprite_2d.flip_h = false
+		
 	if ray_cast_left.is_colliding():
 		direction = 1
+		animated_sprite_2d.flip_h = true
 		
 	position.x += SPEED * delta * direction
